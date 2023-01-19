0dd03d6f243ecd7794f0322f242a3e1683b23b73
knj <rlaskawns888@gmail.com>
2023-01-19 09:38:04 +0900

    wepback cofig

--- gugudanGame/webpack.config.js
+++ gugudanGame/webpack.config.js
@@ -14,11 +14,15 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env','@babel/preset-react'],
-                // plugins: ['@babel/plugin-proposal-class-properties']
+                presets: [
+                    '@babel/preset-env',
+                    '@babel/preset-react',
+                ],
+                plugins: [],
             },
         }],
     },
+    plugins: [],
     output: {        
         filename: 'app.js',
         path: path.join(__dirname, 'dist'),
