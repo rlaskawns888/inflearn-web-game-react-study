d0350b529cdc945cba9cab76b565ea97c54ec998
knj <rlaskawns888@gmail.com>
2023-01-26 16:02:18 +0900

    code정리

--- folloUpGame2/client.jsx
+++ folloUpGame2/client.jsx
@@ -1,9 +1,7 @@
 const React = require('react');
-// const ReactDOM = require('react-dom');
 const ReactDOM = require('react-dom/client');
 
 const FollowUp = require('./followup');
 
-// ReactDOM.render(<FollowUp />, document.querySelector('#root'));
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<FollowUp />);
